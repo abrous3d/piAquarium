f304d195d3b0d31c805282d341ee79a70dae5124
abrous3d <abrous3d@hotmail.com>
2014-12-05 18:23:17 +0200

    FiFO test part 2

--- RaspberryAquaria/Messaging.c
+++ RaspberryAquaria/Messaging.c
@@ -1,7 +1,21 @@
+#include <sys/select.h>
+#include <stdlib.h>
 #include <fcntl.h>
-#include <sys/stat.h>
-#include <sys/types.h>
 #include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <signal.h>
+#include <time.h>
+#include <sys/types.h>
+#include <sys/time.h>
+#include <sys/stat.h>
+#include <errno.h>
+#include <sys/poll.h>
+#include <unistd.h>
+#include "defines.h"
+#include "config.h"
+#include "database.h"
 
 //*********** message FIFO -> server ************ 
 int ToSrv_fd;																// app to Server message fifo handler
@@ -12,6 +26,9 @@ char *FrmServMesFifo = "/tmp/fifoFrmSrv";
 
 #define		MAX_MESS_BUFF		1024
 char buf[MAX_MESS_BUFF];
+char obuf[MAX_MESS_BUFF];
+
+extern REALMEAS Real;	
 //************************************************************************
 
 //************************************************************************
@@ -21,45 +38,40 @@ void Com_Fifo_update(void)
 
 	nbytes = read(FrmSrv_fd,buf, MAX_MESS_BUFF);
 	if(nbytes){
-		write(ToSrv_fd, "Hi", sizeof("Hi"));
+		if(strstr (buf, "#DATA_1") != NULL)
+		{
+			sprintf(obuf,"%0.2f/r/n",Real.TankTemp);
+		}
+	
+		write(ToSrv_fd, obuf, sizeof(obuf));
 	}
 }
-//************************************************************************
+//************************************************************
+//			Initialize communication FIFO
+//************************************************************
 void Com_Fifo_init(void)
 {
-//************************************************************
-	//			Initialize communication FIFO
-	//************************************************************
-//#if 0
-    //mkfifo(ToServMesFifo, 0666);
-	//mkfifo(FromServMesFifo, 0666);
-
-
 	mkfifo(ToServMesFifo, 0666);
 	mkfifo(FrmServMesFifo, 0666);
-
-	//system("mkfifo /tmp/PIPE_FRM_SRV");
-    //system("mkfifo /tmp/PIPE_TO_SRV");
-    // open file descriptors of named pipes to watch
-    ToSrv_fd = open(ToServMesFifo, O_RDWR | O_NONBLOCK);
+ // system("mkfifo /tmp/PIPE_FRM_SRV");
+ // system("mkfifo /tmp/PIPE_TO_SRV");
+ // open file descriptors of named pipes to watch
+  
+	ToSrv_fd = open(ToServMesFifo, O_RDWR | O_NONBLOCK);
     if (ToSrv_fd == -1) {
         perror("open");
         return(1);
-
     }	
 	
-//#endif
-
-
 }
 //************************************************************************
 void Com_Fifo_kill(void)
 {
 	close(ToSrv_fd);
 	close(FrmSrv_fd);
-    /* remove the FIFO */
+ /* remove the FIFO */
     unlink(ToServMesFifo);
-	//unlink(FromServMesFifo);
+ // unlink(FromServMesFifo);
 }
 //************************************************************************
 //************************************************************************
